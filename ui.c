c7c579d6af3bef620dcfe51f42910811878df744
Taha-Elmi <elmi.taha@gmail.com>
2021-02-08 19:28:16 +0330

    feat: "ui" file added

---
+++ ui.c
@@ -0,0 +1,5 @@
+//
+// Created by elmit on 2/8/2021.
+//
+
+#include "ui.h"
---
+++ ui.h
@@ -0,0 +1,13 @@
+//
+// Created by elmit on 2/8/2021.
+//
+
+#ifndef SHIPS_C_UI_H
+#define SHIPS_C_UI_H
+
+#endif //SHIPS_C_UI_H
+
+#include "objects.h"
+
+int menu();
+void draw(map map1);
